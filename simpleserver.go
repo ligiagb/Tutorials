7bc0270a61ee0fd4f680e98c945717b88f375f2e
Ligia Brosch <ligiagb@gmail.com>
2016-09-07 12:57:14 -0700

    added 2 paths (safe and not safe), function that verifies URL just returns safe for now

--- simpleserver.go
+++ simpleserver.go
@@ -2,28 +2,33 @@ package main
 
 import (
     "fmt"
-//    "html"
+    "html"
+    "io"
     "log"
     "net/http"
 )
 
-func main() {
 
 //function that searches the database for malware URLs
 //should search the database using something like this: GET /urlinfo/1/{hostname_and_port}/{original_path_and_query_string}
-
-
+func DataBaseValidation(url string) bool {
+    return true //just getting one response for testing if code is working
+}
 // add a function to create a 2 flows (1 for malware URLs and one for ok URLs)
+func urlinfo(w io.Writer,url string) {
+    validUrl := DataBaseValidation(url)
+    if(validUrl == true) {
+         fmt.Fprintf(w,"URL IS GOOD")
+    } else {
+        fmt.Fprintf(w,"URL IS BAD")
+    }
+}
+func main() {
 
-
-//this is the funtion that makes any typed URL in my local server works, and adds everything after 8081 to be pirnted at the screen
+//this is handling the urlinfo function to handle url requests
    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-    })  
-//  I can comment out his code, nothing breaks, not sure what it does
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hi")
-    }) 
+        urlinfo(w, html.EscapeString(r.URL.Path))
+    })         
 
     log.Fatal(http.ListenAndServe(":8081", nil))
 
