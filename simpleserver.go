bcddf0a374d7bb7ce405365a774ece9584a1c778
Ligia Brosch <ligiagb@gmail.com>
2016-09-08 21:11:42 -0700

    Odd URL lenghts are bad, even ones are good

--- simpleserver.go
+++ simpleserver.go
@@ -6,15 +6,23 @@ import (
     "io"
     "log"
     "net/http"
+    "unicode/utf8"
 )
 
 
 //function that searches the database for malware URLs
 //should search the database using something like this: GET /urlinfo/1/{hostname_and_port}/{original_path_and_query_string}
 func DataBaseValidation(url string) bool {
-    return true //just getting one response for testing if code is working
+    // if function to consider all websites with an odd number of character as malware
+   urlLength := utf8.RuneCountInString(html.EscapeString(url))
+    if urlLength%2==0 {
+        return true
+    } else {
+        return false
+    }
+
 }
-// add a function to create a 2 flows (1 for malware URLs and one for ok URLs)
+//  function with 2 flows depending on the URL requested (one for malware URLs and one for ok URLs)
 func urlinfo(w io.Writer,url string) {
     validUrl := DataBaseValidation(url)
     if(validUrl == true) {
