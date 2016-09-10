6777ec9bdc2f2ffc10f4cf3f621a59145a159dfb
Ligia Brosch <ligiagb@gmail.com>
2016-09-10 00:08:45 -0700

    Got json in, it compiles, but throws 404 error

--- simpleserver.go
+++ simpleserver.go
@@ -1,49 +1,62 @@
 package main
 
 import (
-    "fmt"
-    "html"
-    "io"
-    "log"
-    "net/http"
-    "unicode/utf8"
+	"encoding/json"
+	"html"
+	"log"
+	"net/http"
+	"unicode/utf8"
+
+	"github.com/gorilla/mux"
 )
+
 //json to tell the client website is safe and return the same URL - I probably have to save the URL requested somewhere
 //took from https://tutorialedge.net/creating-simple-restful-json-api-with-go and just added jsons doesn't break it
-//TO DO:figure out how routers work and why I can't install it
+//TO DO:figure out how routers work
 type ValidationResponce struct {
-	OriginalURL string `json:"url"`    
+	OriginalURL string `json:"url"`
 	FlaggedURL  bool   `json:"flagged"` //not sure if should go for the safe or unsafe one here
-	
+
 }
+
 //function that searches the database for malware URLs
 //should search the database using something like this: GET /urlinfo/1/{hostname_and_port}/{original_path_and_query_string}
-func DataBaseValidation(url string) bool {
-    // if function to consider all websites with an odd number of character as malware
-   urlLength := utf8.RuneCountInString(html.EscapeString(url))
-    if urlLength%2==0 {
-        return true
-    } else {
-        return false
-    }
+func DatabaseValidation(url string) bool {
+	// if function to consider all websites with an odd number of character as malware
+	urlLength := utf8.RuneCountInString(html.EscapeString(url))
+	if urlLength%2 == 0 {
+		return true
+	} else {
+		return false
+	}
 
 }
+func JsonResponse(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Path
+	UnsafeUrl := DatabaseValidation(url)
+	Response := ValidationResponce{OriginalURL: url, FlaggedURL: UnsafeUrl}
+	if err := json.NewEncoder(w).Encode(Response); err != nil {
+		panic(err)
+	}
+}
+
 //  function with 2 flows depending on the URL requested (one for malware URLs and one for ok URLs)
-func urlinfo(w io.Writer,url string) {
-    validUrl := DataBaseValidation(url)
-    if(validUrl == true) {
-         fmt.Fprintf(w,"URL IS GOOD")
-    } else {
-        fmt.Fprintf(w,"URL IS BAD")
-    }
-}
+/*func urlinfo(w io.Writer, url string) {
+	UnsafeUrl := DatabaseValidation(url)
+	if UnsafeUrl == true {
+		fmt.Fprintf(w, "URL IS BAD, please try another URL")
+	} else {
+		fmt.Fprintf(w, "URL IS GOOD")
+	}
+
+}*/
 func main() {
-
-//this is handling the urlinfo function to handle url requests
-   http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        urlinfo(w, html.EscapeString(r.URL.Path))
-    })         
-
-    log.Fatal(http.ListenAndServe(":8081", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	//this is handling the urlinfo function to handle url requests
+	/*	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		urlinfo(w, html.EscapeString(r.URL.Path))
+	}) */
+	router.HandleFunc("/", JsonResponse)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 
 }
